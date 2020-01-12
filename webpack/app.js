bcdf1ed8d174bba24adec6ccc87ee96a1d5688df
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-01-11 21:42:02 -0500

    Added portal vue plugin

--- webpack/app.js
+++ webpack/app.js
@@ -7,12 +7,16 @@ import "./all.websom-styles";
 
 import WebsomVue from "./vue.plugin";
 
+import PortalVue from "portal-vue";
+
 import "normalize.css";
 
 export function createApp () {
 	const router = createRouter(Packages);
 
 	Vue.use(WebsomVue);
+
+	Vue.use(PortalVue);
 
 	for (let v of Packages) {
 		Vue.component(v.info.name, v.vue);
