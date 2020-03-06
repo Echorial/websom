35450b001f918fde675134ae52b884fac20841d1
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-03-06 17:35:55 -0500

    Added bundleAnalyzer and compression to webpack.

--- webpack/webpack.client.config.js
+++ webpack/webpack.client.config.js
@@ -3,6 +3,9 @@ const merge = require("webpack-merge");
 const baseConfig = require("./webpack.config.js");
 const VueSSRClientPlugin = require("vue-server-renderer/client-plugin");
 
+const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = (websomServer) => {
 	return merge(baseConfig(websomServer), {
 		entry: "./entry.js",
@@ -13,7 +16,18 @@ module.exports = (websomServer) => {
 			}
 		},*/
 		plugins: [
-			new VueSSRClientPlugin()
+			new VueSSRClientPlugin(),
+			new CompressionPlugin({
+				filename: '[path].gz[query]',
+				algorithm: 'gzip',
+				test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+				threshold: 10240,
+				minRatio: 0.8
+			}),
+			new BundleAnalyzerPlugin({
+				openAnalyzer: false,
+				analyzerPort: 8971
+			})
 		],
 		output: {
 			
