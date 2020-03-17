90c0ba631256a712d4b6de751faff89d5e73444e
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-03-17 14:03:21 -0400

    Added session tracking to websom.fetch.

--- webpack/websom-client/fetch.js
+++ webpack/websom-client/fetch.js
@@ -1,18 +1,37 @@
 export default (store) => {
 	return (route, body, opts) => {
+		if (typeof window === "undefined")
+			return {status: "error", message: "Fetch request from server"};
+		
 		opts = opts || {};
+
+		let session = sessionStorage.getItem("Websom-Session");
+
+		let addHeaders = {};
+
+		if (session) {
+			addHeaders["X-Session"] = session;
+		}
 		
 		return fetch(store.state.websom.api + route, {
 			method: opts.method || "POST",
 			body: JSON.stringify(body),
+			credentials: "same-origin",
 			headers: {
 				"Content-Type": "application/json",
-				...(opts.headers || {})
+				...(opts.headers || {}),
+				...addHeaders
 			}
-		}).then(raw => raw.json()).catch(() => {console.error("Invalid endpoint " + route)}).then((data) => {
-			return new Promise((res, rej) => {
-				res(data);
+		}).then(res => {
+			if (res.headers.get("X-Set-Session")) {
+				sessionStorage.setItem("Websom-Session", res.headers.get("X-Set-Session"));
+			}
+
+			return res.json().then((data) => {
+				return new Promise((resolve, rej) => {
+					resolve(data);
+				});
 			});
-		});
+		}).catch(() => {console.error("Invalid endpoint " + route)});
 	};
 };
