cff4a053caad29925738eccf939c5d8f3fc0c74b
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-03-23 12:17:46 -0400

    Fixed missing deploy ui servers.

--- platform/node/index.js
+++ platform/node/index.js
@@ -5,7 +5,8 @@ const createServer = require("../../webpack/server");
 async function startWebsomDevelopmentServer(server, port, apiPort) {
 	let expressApp = await createServer(server, "http://localhost:" + apiPort);
 
-	expressApp.listen(port);
+	if (expressApp)
+		expressApp.listen(port);
 
 	console.log(`Started server on port ${port}`);
 	
