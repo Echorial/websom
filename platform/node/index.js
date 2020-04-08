f7c8183263ba33cf9b7c2a4b108bbfc7495d54cf
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-04-08 12:25:18 -0400

    Added executeDeploy export to node builds.

--- platform/node/index.js
+++ platform/node/index.js
@@ -1,6 +1,7 @@
 const open = require("open");
 
 const createServer = require("../../webpack/server");
+const doDeploy = require("../../webpack/deploy");
 
 async function startWebsomDevelopmentServer(server, port, apiPort) {
 	let expressApp = await createServer(server, "http://localhost:" + apiPort);
@@ -10,10 +11,15 @@ async function startWebsomDevelopmentServer(server, port, apiPort) {
 
 	console.log(`Started server on port ${port}`);
 	
-	if (server.config.openInBrowser)
+	if (server().config.openInBrowser)
 		open("http://localhost:" + port + "/");
 }
 
+async function executeDeploy(server, deploy) {
+	return await doDeploy(server, deploy);
+}
+
 module.exports = {
-	startWebsomDevelopmentServer
+	startWebsomDevelopmentServer,
+	executeDeploy
 };
