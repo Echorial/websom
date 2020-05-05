36b84bfe7bec06d53ceffa770ca7bbf270b0836b
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-05-04 22:02:45 -0400

    Force entity id toString.

--- webpack/websom-client/entity.js
+++ webpack/websom-client/entity.js
@@ -4,7 +4,11 @@ export default class Entity {
 
 		this.$fields = [];
 
+		if (typeof data.id !== "undefined")
+			data.id = data.id.toString();
+
 		for (let k in data) if (data.hasOwnProperty(k)) { this[k] = data[k]; this.$fields.push(k); }
+
 
 		this.$collection = data ? collection : "/";
 	}
