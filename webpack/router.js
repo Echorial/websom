9c783796693cece038303458af167c9cb9d3a006
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-05-04 22:01:38 -0400

    Added infinite route nesting.

--- webpack/router.js
+++ webpack/router.js
@@ -13,10 +13,14 @@ export function createRouter (views) {
 		if (view.info.nested) {
 			if (!nested[view.info.nested]) nested[view.info.nested] = [];
 
-			nested[view.info.nested].push({
+			let sub = {
 				path: view.info.route,
 				component: view.vue
-			});
+			};
+
+			nested[view.info.nested].push(sub);
+
+			view.$nested = sub;
 		}
 	}
 
@@ -30,10 +34,20 @@ export function createRouter (views) {
 				component: view.vue
 			};
 
-			if (nested[view.info.route])
-				route.children = nested[view.info.route];
+			let absoluteRoute = view.info.route;
+
+			if (nested[absoluteRoute])
+				route.children = nested[absoluteRoute];
 
 			routes.push(route);
+		}else if (view.info.type == "page" && view.info.nested) {
+			let absoluteRoute = view.info.nested + "/" + view.info.route;
+
+			if (nested[absoluteRoute]) {
+				let myNested = view.$nested;
+
+				myNested.children = nested[absoluteRoute];
+			}
 		}
 	}
 
