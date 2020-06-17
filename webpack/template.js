cc6d526b79b444ae10df2fd0a9486e3b9bfd5990
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-06-17 15:53:01 -0400

    Added more meta information.

--- webpack/template.js
+++ webpack/template.js
@@ -3,8 +3,11 @@ module.exports = (data) =>
 <!DOCTYPE html>
 <html lang="${data.lang || "en"}">
 	<head>
+		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="{{metaDescription}}">
+		<link rel="canonical" href="{{canonicalURL}}" />
+		{{{ headElements }}}
 		{{{ renderResourceHints() }}}
 		{{{ renderStyles() }}}
 		<title>{{title}}</title>
