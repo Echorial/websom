e16a8b3eeb768557f8d8f65e6b75367c9f191be2
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-06-17 15:32:24 -0400

    Added favicon builds.

---
+++ base-theme/build.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const pngToIco = require("png-to-ico");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = async (context) => {
+	await Promise.all([new Promise((res) => {
+		let rIcon = context.server.configService.getString("theme.theme.app", "rasterIcon");
+
+		if (rIcon) {
+			pngToIco(path.resolve(context.server.config.root, rIcon)).then(buf => {
+				fs.writeFileSync(path.resolve(path.dirname(path.resolve(context.server.config.root, rIcon)), "favicon.ico"), buf);
+				res();
+			}).catch((err) => {
+				console.log("Error while generating ICO", err);
+				res();
+			});
+		}else{
+			res();
+		}
+	}), new Promise((res) => {
+		let vIcon = context.server.configService.getString("theme.theme.app", "vectorIcon");
+
+		if (vIcon) {
+			//fs.writeFileSync(path.resolve(context.dist, "favicon.svg"), fs.readFileSync(path.resolve(context.server.config.root, vIcon)));
+		}
+
+		res();
+	})]);
+
+	const CopyPlugin = require('copy-webpack-plugin');
+	let pats = [];
+	let rIcon = context.server.configService.getString("theme.theme.app", "rasterIcon");
+
+	if (rIcon) {
+		console.log(path.resolve(path.dirname(path.resolve(context.server.config.root, rIcon)), "favicon.ico"));
+		pats.push({
+			from: path.resolve(path.dirname(path.resolve(context.server.config.root, rIcon)), "favicon.ico")
+		});
+	}
+
+	let vIcon = context.server.configService.getString("theme.theme.app", "vectorIcon");
+
+	if (vIcon)
+		pats.push({
+			from: path.resolve(context.server.config.root, vIcon),
+			to: "favicon.svg"
+		});
+
+	if (pats.length > 0) {
+		let conf = {
+			webpack: {
+				plugins: [
+					new CopyPlugin({
+						patterns: pats
+					})
+				]
+			}
+		};
+		
+		return {
+			server: conf,
+			client: conf
+		};
+	}else
+		return {};
+};
