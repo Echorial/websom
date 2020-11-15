6c01877f7172e32df1c827c3627c7875007bf96d
AskingQuestions <18402381+AskingQuestions@users.noreply.github.com>
2020-11-15 14:12:58 -0500

    Aded out dir to static

--- webpack/deploys/static.js
+++ webpack/deploys/static.js
@@ -4,33 +4,40 @@ const fs = require("fs");
 const sharp = require("sharp");
 
 module.exports = async ({ server: websomServer, deploy, bundles, template, createBundleRenderer}) => {
+	let outDir = path.resolve(websomServer.config.root, "../" + (deploy.path || "dist_deploy"));
+	console.log("Building to " + outDir);
+
 	let server = {
 		webpack: {
 			output: {
-				publicPath: "./"
+				publicPath: "./",
+				path: outDir
 			}
 		}
 	};
+
+	console.log("API: " + JSON.stringify(deploy.api || websomServer.apiHost));
 
 	let client = {
 		webpack: {
 			mode: "production",
 			plugins: [
 				new webpack.DefinePlugin({
-					__websom_api: JSON.stringify(websomServer.apiHost),
+					__websom_api: JSON.stringify(deploy.api || websomServer.apiHost),
 					__websom_data: JSON.stringify(await websomServer.configService.computeClientData(null)),
 					__websom_static: "true"
 				})
 			],
 			output: {
 				filename: "[name].js",
-				publicPath: "/"
+				publicPath: "/",
+				path: outDir
 			}
 		},
 		hooks: {
 			done() {
 
-				let filename = path.resolve(websomServer.config.javascriptOutput, "index.html");
+				let filename = path.resolve(outDir, "index.html");
 				fs.writeFileSync(filename, `
 					<!DOCTYPE html>
 					<html lang="${deploy.lang || 'en'}">
